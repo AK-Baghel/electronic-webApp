9c905997b7fbcfe03217accc612e96c586c74523
Ankit BagheL <ak9664773@gmail.com>
2023-10-21 20:39:13 +0530

    add marquee and 1 section in home.jsx

--- src/component/carousel/Carousel.jsx
+++ src/component/carousel/Carousel.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-// import { CCarousel } from "@coreui/react"
-// import { CCarouselItem ,CImage} from "@coreui/react"
 import "./style.css"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,11 +8,20 @@ import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 function Carousel() {
+
+    const data = [
+        "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+    ];
+    
     return (
         <Swiper
             spaceBetween={0}
             centeredSlides={true}
-            
+
             autoplay={{
                 delay: 2500,
                 disableOnInteraction: false,
@@ -26,12 +33,17 @@ function Carousel() {
             modules={[Autoplay, Pagination, Navigation]}
             className="mySwiper"
         >
-            <SwiperSlide><img className='new' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide><img className='new' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide><img className='new' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" /></SwiperSlide>
+
+            {
+                data.map((item) => {
+                    return (
+                        <SwiperSlide><img className='new' src={item} alt="" /></SwiperSlide>
+                    )
+                })
+            }
+
         </Swiper>
     )
 }
 
 export default Carousel
-{/* <CImage className="d-block w-80 new" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="slide 1" /> */ }
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import Carousel from '../carousel/Carousel'
+import MarqueeComp from '../marquee/MarqueeComp'
+import "./style.css"
+
 function Home() {
+
   return (
     <div>
       <Carousel />
+
+      <div className="aboutContainer homeContainer">
+        <div className="aboutTrans contactBox1">
+          <div className=" homeText">Glorious Electronics- Absolute solidity under extreme condition</div>
+          <div className="aboutTransDesc">Glorious Electronics is an authorized and independent distributor of electronic components. Glorious Electronics was set up over 75 years ago and is located at lamington road, Mumbai the heart of Electronic market with founder vision to supply genuine quality & reliable electronic components. Glorious Electronics is an authorized distributor of Cosmo and TDK.</div>
+        </div>
+        <MarqueeComp />
+      </div>
+
     </div>
   )
 }
